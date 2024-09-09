00638f005b206625fb7cb8cf64b4527391a8dde2
Cravcenco Dmitrii <dima16kravchenko@gmail.com>
2024-09-09 14:22:37 +0300

    Google auth

---
+++ src/main/java/com/app/lovemusic/LoveMusicApplication.java
@@ -0,0 +1,13 @@
+package com.app.lovemusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveMusicApplication.class, args);
+    }
+
+}
