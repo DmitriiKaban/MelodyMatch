de6d136ad4ec775c29a23b7fd6d41f439d240f70
Cravcenco Dmitrii <dima16kravchenko@gmail.com>
2024-09-25 22:58:31 +0300

    add jdbc template

--- src/main/java/com/app/lovemusic/LoveMusicApplication.java
+++ src/main/java/com/app/lovemusic/LoveMusicApplication.java
@@ -1,13 +1,52 @@
 package com.app.lovemusic;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 public class LoveMusicApplication {
 
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+
+    @Value("${spring.datasource.username}")
+    private String datasourceUsername;
+
+    @Value("${spring.datasource.password}")
+    private String datasourcePassword;
+
     public static void main(String[] args) {
         SpringApplication.run(LoveMusicApplication.class, args);
+    }
+
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+    @Bean
+    public DataSource dataSource() {
+
+        PoolProperties p = new PoolProperties();
+        p.setUrl(datasourceUrl);
+        // set mysql driver
+        p.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        p.setUsername(datasourceUsername);
+        p.setPassword(datasourcePassword);
+        DataSource datasource = new DataSource();
+        datasource.setPoolProperties(p);
+        return datasource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 
 }
