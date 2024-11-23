ae0785627cc1e4aeac8b8fa96696aa17ec0ac0b1
valeriafz <valeria.frunza33@gmail.com>
2024-11-23 22:42:22 +0200

    add csp secure headers

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,5 +12,9 @@ export default defineConfig({
         secure: false,
       },
     },
+    headers: {
+      "Content-Security-Policy":
+        "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self'; font-src 'self'; object-src 'none';",
+    },
   },
 });
