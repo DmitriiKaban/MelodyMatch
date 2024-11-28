0fc7021202acab04e14f19baa2becf1edb812314
valeriafz <valeria.frunza33@gmail.com>
2024-11-28 22:05:28 +0200

    fix csp rules

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,5 +12,9 @@ export default defineConfig({
         secure: false,
       },
     },
+    headers: {
+      "Content-Security-Policy":
+        "default-src 'self'; script-src 'self' 'unsafe-inline' https://accounts.google.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob: https://images.pexels.com; connect-src 'self'; object-src 'none';",
+    },
   },
 });
