ef7c57cbf4c96316d9706b560ce7f1d75315cf62
valeriafz <valeria.frunza33@gmail.com>
2024-11-29 14:22:49 +0200

    add clientID for google oauth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-ReactDOM.render(
-  // clientId needs to be added
-  <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">  
-    <App />
-  </GoogleOAuthProvider>,
-  document.getElementById('root')
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>
 );
