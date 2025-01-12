bec8ff0a121e037129c574d62bcab831341d2586
valeriafz <valeria.frunza33@gmail.com>
2025-01-12 13:06:52 +0200

    fix headers

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,11 +14,7 @@ export default defineConfig({
     },
     headers: {
       "Content-Security-Policy":
-<<<<<<< HEAD
         "default-src 'self'; script-src 'self' 'unsafe-inline' https://accounts.google.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob: https://images.pexels.com; connect-src 'self'; object-src 'none';",
-=======
-        "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self'; font-src 'self'; object-src 'none';",
->>>>>>> a583a3ceb89f941d934a40de3248f74e9f85b2e9
     },
   },
 });
