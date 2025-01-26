a0943def9373771b3d8639a295e9e9de7efc4e38
valeriafz <valeria.frunza33@gmail.com>
2025-01-26 18:38:11 +0200

    fix token issue

--- frontend/src/utils/newRequest.js
+++ frontend/src/utils/newRequest.js
@@ -1,17 +1,25 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "/auth",
-  // withCredentials: true,
+  baseURL: "/auth", // Proxy points this to the backend
+  // withCredentials: true, // Uncomment if your backend uses cookies
 });
+
 
 newRequest.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("authToken");
+
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+
+    if (token.split(".").length === 3) {
+      config.headers.Authorization = `Bearer ${token}`;
+    } else {
+      console.error("Invalid token format:", token);
+      localStorage.removeItem("authToken"); 
+    }
   }
+
   return config;
 });
-
 
 export default newRequest;
